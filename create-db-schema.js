7b6fea300b9296127d57107392af4ba7d72b5fc3
Your Name <you@example.com>
2023-09-21 16:47:53 -0500

    1.0 of the somnus db app

--- create-db-schema.js
+++ create-db-schema.js
@@ -1,25 +1,67 @@
 import { MongoClient } from "mongodb";
+import _ from "lodash";
 
 const mongoUrl = process.env.MONGO_URL || "localhost";
 const mongoPort = process.env.MONGO_PORT || "27017";
 const url = `mongodb://${mongoUrl}:${mongoPort}`;
 const client = new MongoClient(url, {
-  connectTimeoutMS: 5000,
-  serverSelectionTimeoutMS: 5000
+    connectTimeoutMS: 5000,
+    serverSelectionTimeoutMS: 5000,
 });
 const dbName = "sleeper";
+const collectionNames = ["leagues", "rosters", "users", "matchups"];
+
+console.log("\n1. try to connect to mongo instance");
 try {
-  await client.connect();
-  await client.db("admin").command({ping: 1});
-  console.log("client connected");
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("client connected successfully");
 } catch (err) {
-  console.log(err);
-  return;
+    console.log(err);
 }
 
+console.log("\n2. check database status");
 try {
-    const db = client.db(dbName);
+    const db = await client.db("admin");
+    const databases = await db.admin().listDatabases();
+    const databaseExists = databases.databases.find((val, idx) => {
+        return val.name === dbName;
+    });
+    if (!databaseExists) {
+        console.log(
+            "WARN: the desired database is not present in the instance."
+        );
+        console.log("\n2.1. Create database");
+        const newDb = await client.db(dbName);
+        await newDb.createCollection(collectionNames[0]);
+        console.log("the desired database was added.");
+    } else {
+        console.log("the desired database is present in the instance.");
+    }
+} catch (err) {
+    console.log(err);
 }
-catch(err) {console.log(err)}
 
-db.coll
+console.log("\n3. check collections in database");
+try {
+    const db = await client.db(dbName);
+    const collections = await db.listCollections();
+    const collectionsPresent = (await collections.toArray()).map((a) => a.name);
+    //console.log(_.difference(collectionNames, collectionsPresent))
+    const collectionsToCreate = _.difference(
+        collectionNames,
+        collectionsPresent
+    );
+    console.log(
+        "the following collections need to be created: ",
+        collectionsToCreate
+    );
+	for(let i = 0; i < collectionsToCreate.length; i++) {
+		await db.createCollection(collectionsToCreate[i])
+	}
+} catch (err) {
+    console.log(err);
+}
+
+console.log("script complete");
+client.close();
--- rebuild-db.js
+++ rebuild-db.js
@@ -1,84 +1,188 @@
 //rebuild the database from scratch
-const https = require("https");
-const USER_ID = ""
+import https from "https";
+import { MongoClient } from "mongodb";
+import secrets from "./secrets.json" assert { type: "json"};
+const USER_ID = secrets.SLEEPER_USER_ID;
 
 const FIRST_LEAGUE_YEAR = 2020;
 
+const mongoUrl = process.env.MONGO_URL || "localhost";
+const mongoPort = process.env.MONGO_PORT || "27017";
+const url = `mongodb://${mongoUrl}:${mongoPort}`;
+const client = new MongoClient(url, {
+    connectTimeoutMS: 5000,
+    serverSelectionTimeoutMS: 5000,
+});
+
 //util function
 function getData(url) {
-  return new Promise((resolve, rej) => {
-    https.get(url, (res) => {
-      let data = "";
+    return new Promise((resolve, rej) => {
+        https.get(url, (res) => {
+            let data = "";
 
-      // A chunk of data has been received.
-      res.on("data", (chunk) => {
-        data += chunk;
-      });
+            // A chunk of data has been received.
+            res.on("data", (chunk) => {
+                data += chunk;
+            });
 
-      res.on("end", () => {resolve(JSON.parse(data))});
+            res.on("end", () => {
+                resolve(JSON.parse(data));
+            });
+        });
     });
-  });
 }
 
 async function init() {
-  //1. get status of the irl nfl
-  const currentNflState = await getData("https://api.sleeper.app/v1/state/nfl");
-  console.log("*** NFL STATE ***");
-  console.log("Season: " + currentNflState.season);
-  const CURRENT_NFL_SEASON = parseInt(currentNflState.season);
-  console.log("Week: " + currentNflState.week);
-  console.log("Season starts on: " + currentNflState.season_start_date);
+    //1. get status of the irl nfl
+    const currentNflState = await getData(
+        "https://api.sleeper.app/v1/state/nfl"
+    );
+    console.log("*** NFL STATE ***");
+    console.log("Season: " + currentNflState.season);
+    const CURRENT_NFL_SEASON = parseInt(currentNflState.season);
+    console.log("Week: " + currentNflState.week);
+    console.log("Season starts on: " + currentNflState.season_start_date);
 
-  //2 need to get all historical leagues/seasons.
-  console.log("\n*** HISTORICAL LEAGUES ***")
+    //2 need to get all historical leagues/seasons.
+    console.log("\n*** HISTORICAL LEAGUES ***");
 
-  //2.1 compile a list of all leagues between 2020 and the current year
-  const historicalLeagueIds = [];
-  //2.1.1 find the first league. started in 2020. league name cannot change so we search by that
-  console.log("Looking for the initial league from the " + FIRST_LEAGUE_YEAR + " season...")
-  const firstLeagueData = await getData(`https://api.sleeper.app/v1/user/${USER_ID}/leagues/nfl/${FIRST_LEAGUE_YEAR}`);
-  const initialLeague = firstLeagueData.find((league)=>{
-    return league.name === "Dynasty league 2020"
-  });
-  if(!initialLeague) {
-    console.log("\n*** ERROR: could not find the initial league. Exiting. ***")
-    return;
-  }
-  const initialLeagueId = initialLeague.league_id;
-  historicalLeagueIds.push(initialLeagueId);
-  console.log("Found the league from year " + FIRST_LEAGUE_YEAR);
+    //2.1 compile a list of all leagues between 2020 and the current year
+    const historicalLeagueIds = [];
+    //2.1.1 find the first league. started in 2020. league name cannot change so we search by that
+    console.log(
+        "Looking for the initial league from the " +
+            FIRST_LEAGUE_YEAR +
+            " season..."
+    );
+    const firstLeagueData = await getData(
+        `https://api.sleeper.app/v1/user/${USER_ID}/leagues/nfl/${FIRST_LEAGUE_YEAR}`
+    );
+    const initialLeague = firstLeagueData.find((league) => {
+        return league.name === "Dynasty league 2020";
+    });
+    if (!initialLeague) {
+        console.log(
+            "\n*** ERROR: could not find the initial league. Exiting. ***"
+        );
+        return;
+    }
+    const initialLeagueId = initialLeague.league_id;
+    historicalLeagueIds.push(initialLeagueId);
+    console.log("Found the league from year " + FIRST_LEAGUE_YEAR);
 
-  //2.1.2 get the rest of the leagues thru the current irl year using the previous league's id
+    //2.1.2 get the rest of the leagues thru the current irl year using the previous league's id
 
-  let i = 1;
-  while(FIRST_LEAGUE_YEAR + i <= CURRENT_NFL_SEASON) {
-    console.log("Looking for the next league from the " + (FIRST_LEAGUE_YEAR + i) + " season...")
-    let historicalLeagueData = await getData(`https://api.sleeper.app/v1/user/${USER_ID}/leagues/nfl/${FIRST_LEAGUE_YEAR + i}`);
-    if(!historicalLeagueData) {
-        console.log("The leagues for the current NFL season, " + (FIRST_LEAGUE_YEAR + i) + ", have not yet been created.")
-        break;
+    let i = 1;
+    while (FIRST_LEAGUE_YEAR + i <= CURRENT_NFL_SEASON) {
+        console.log(
+            "Looking for the next league from the " +
+                (FIRST_LEAGUE_YEAR + i) +
+                " season..."
+        );
+        let historicalLeagueData = await getData(
+            `https://api.sleeper.app/v1/user/${USER_ID}/leagues/nfl/${
+                FIRST_LEAGUE_YEAR + i
+            }`
+        );
+        if (!historicalLeagueData) {
+            console.log(
+                "The leagues for the current NFL season, " +
+                    (FIRST_LEAGUE_YEAR + i) +
+                    ", have not yet been created."
+            );
+            break;
+        }
+
+        const theNextLeague = historicalLeagueData.find((league) => {
+            return (
+                league.previous_league_id ===
+                historicalLeagueIds[historicalLeagueIds.length - 1]
+            );
+        });
+
+        if (!theNextLeague) {
+            console.log(
+                "An error occurred finding the next league, for year " +
+                    (FIRST_LEAGUE_YEAR + i)
+            );
+            break;
+        }
+        console.log("Found the league from year " + (FIRST_LEAGUE_YEAR + i));
+        historicalLeagueIds.push(theNextLeague.league_id);
+        i++;
     }
 
-    const theNextLeague = historicalLeagueData.find((league)=>{
-        return league.previous_league_id === historicalLeagueIds[historicalLeagueIds.length - 1];
-    })
-
-    if(!theNextLeague) {
-        console.log("An error occurred finding the next league, for year " + (FIRST_LEAGUE_YEAR + i));
-        break;
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("client connected successfully");
+    } catch (err) {
+        console.log(err);
     }
-    console.log("Found the league from year " + (FIRST_LEAGUE_YEAR + i))
-    historicalLeagueIds.push(theNextLeague.league_id)
-    i++;
-  }
 
+    const sleeperDb = await client.db("sleeper");
 
-  //3. get info about fantasy leagues (each year is considered an individual league, linked together via previous_league_id)
-  const leagueYear = await getData(`https://api.sleeper.app/v1/league/${historicalLeagueIds[historicalLeagueIds.length - 1]}`);
-  console.log("\n*** CURRENT LEAGUE STATE ***");
-  console.log("League season: " + leagueYear.season);
-  console.log("This league matches current NFL year: " + (leagueYear.season == currentNflState.season))
-  console.log("League status: " + leagueYear.status)
+    //3.1 load league data
+    console.log("loading leagues data");
+    for (let i = 0; i < historicalLeagueIds.length; i++) {
+        const sleeperLeagueData = await getData(
+            `https://api.sleeper.app/v1/league/${historicalLeagueIds[i]}`
+        );
+        await sleeperDb.collection("leagues").insertOne(sleeperLeagueData);
+    }
+    console.log("leagues data loading complete");
+
+    //3.2 load roster data
+    console.log("loading rosters data.");
+    for (let i = 0; i < historicalLeagueIds.length; i++) {
+        const sleeperRostersData = await getData(
+            `https://api.sleeper.app/v1/league/${historicalLeagueIds[i]}/rosters`
+        );
+        for (let j = 0; j < sleeperRostersData.length; j++) {
+            await sleeperDb
+                .collection("rosters")
+                .insertOne(sleeperRostersData[j]);
+        }
+    }
+    console.log("roster data loading complete.");
+
+    //3.3 load user data
+    console.log("loading user data");
+    for (let i = 0; i < historicalLeagueIds.length; i++) {
+        const sleeperUsersData = await getData(
+            `https://api.sleeper.app/v1/league/${historicalLeagueIds[i]}/users`
+        );
+        for (let j = 0; j < sleeperUsersData.length; j++) {
+            sleeperUsersData[j].league_id = historicalLeagueIds[i];
+            await sleeperDb.collection("users").insertOne(sleeperUsersData[j]);
+        }
+    }
+    console.log("user data loading complete");
+
+    //3.4 load matchups data
+    //this one is alittle tougher because the api call requires you to specify a week number, so we need to know how many weeks were in any given season
+    console.log("loading matchup data");
+    for (let i = 0; i < historicalLeagueIds.length; i++) {
+        const leagueId = historicalLeagueIds[i];
+        const theLeague = await sleeperDb
+            .collection("leagues")
+            .findOne({ league_id: leagueId });
+        const weeksInLeague =
+            parseInt(theLeague.settings.playoff_week_start) - 1;
+        for (let j = 1; j <= weeksInLeague; j++) {
+            const weeklyMatchupData = await getData(
+                `https://api.sleeper.app/v1/league/${leagueId}/matchups/${j}`
+            );
+            for(let k = 0; k < weeklyMatchupData.length; k++){
+                weeklyMatchupData[k].week = j;
+                weeklyMatchupData[k].league_id = leagueId;
+                await sleeperDb.collection("matchups").insertOne(weeklyMatchupData[k]);
+            }
+        }
+    }
+    console.log("matchup data loading complete.")
+
+    client.close();
 }
 
 init();
